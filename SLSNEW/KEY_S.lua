96c93f3bc459ada744e3464e54ac497d65259af9
nothingff <170884425+IX-I@users.noreply.github.com>
2025-12-26 19:23:53 +0100

    Implement loading screen and UI notifications
    Added loading screen handling and notifications.

--- SLSNEW/KEY_S.lua
+++ SLSNEW/KEY_S.lua
@@ -1,3 +1,24 @@
+local player = game.Players.LocalPlayer
+local loadingScreen = player:WaitForChild("PlayerGui"):FindFirstChild("LoadingScreen")
+
+for i = 1, 20 do
+    if loadingScreen and loadingScreen.Parent then
+        loadingScreen:Destroy()
+    else
+        
+        break 
+    end
+end
+local coreGui = game:GetService("CoreGui")
+if coreGui:FindFirstChild("ScreenGui") then
+    Fluent:Notify({
+        Title = "NOTHING X",
+        Content = "UI RUNING",
+        SubContent = "", 
+        Duration = 5
+    })
+    return
+end
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
 local Window = Luna:CreateWindow({
     Name = " ", 
@@ -26,3 +47,11 @@ local Window = Luna:CreateWindow({
     }
 })
 Luna:Destroy()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IX-I/F/refs/heads/W/SLSNEW/UI.lua"))()
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IX-I/F/refs/heads/W/SLSNEW/THEME_UI.lua"))()
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/IX-I/F/refs/heads/W/SLSNEW/delete"))()
+end)
