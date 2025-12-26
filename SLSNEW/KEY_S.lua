4a6e82c5ce8e10b715a8c4551270048fdef3719d
nothingff <170884425+IX-I@users.noreply.github.com>
2025-12-26 18:37:46 +0100

    Update KEY_S.lua

--- SLSNEW/KEY_S.lua
+++ SLSNEW/KEY_S.lua
@@ -1,1 +1,28 @@
-
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
+local Window = Luna:CreateWindow({
+    Name = " ", 
+    Subtitle = " ", 
+    LogoID = "", 
+    LoadingEnabled = true, 
+    LoadingTitle = "NOTHING X", 
+    LoadingSubtitle = "", 
+    ConfigSettings = {
+        RootFolder = nil, 
+        ConfigFolder = "KeyX"
+    },
+    KeySystem = true, 
+    KeySettings = {
+        Title = "NOTHING X HUB",
+        Subtitle = "Key System",
+        Note = "",
+        SaveInRoot = false, 
+        SaveKey = true, 
+        Key = {"7793229829785927986293597837322924345997"},
+        SecondAction = {
+            Enabled = true,
+            Type = "Link", 
+            Parameter = "https://xnothing0x.github.io/Key/"
+        }
+    }
+})
+Luna:Destroy()
