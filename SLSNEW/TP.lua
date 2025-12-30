cc52ec2047c7e6481371e9cf6f4d5121edfdf4e4
nothingff <170884425+IX-I@users.noreply.github.com>
2025-12-30 20:59:17 +0100

    Update TP.lua

--- SLSNEW/TP.lua
+++ SLSNEW/TP.lua
@@ -1,1 +1,4 @@
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = (game.Players.LocalPlayer.Team.Name == "Home" and CFrame.new(-16, 11, -214) or CFrame.new(-17, 11, -255))
+local p = game.Players.LocalPlayer
+local t = p.Team and p.Team.Name
+local c = game.PlaceId == 12177325772 and {CFrame.new(-17,11,-250), CFrame.new(-17,11,-217)} or {CFrame.new(-17,11,-255), CFrame.new(-16,11,-214)}
+p.Character.HumanoidRootPart.CFrame = t == "Home" and c[2] or t == "Away" and c[1] or p.Character.HumanoidRootPart.CFrame
